ce0ce68a2d9c9e1d6d13e388cc25d8c76eb34326
Kojo-Brown <100282824+Kojo-Brown@users.noreply.github.com>
2026-07-23 05:59:35 -0400

    Initial Commit

---
+++ duesday/duesdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  duesdayApp.swift
+//  duesday
+//
+//  Created by Nicholas Darko Brown on 7/23/26.
+//
+
+import SwiftUI
+
+@main
+struct duesdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
